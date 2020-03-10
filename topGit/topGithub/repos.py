a67cd4f60562fb3cdb81c824ed5417470d74ff75
saurabhb44 <saurabhbhardwaj379@gmail.com>
2020-03-10 18:18:53 +0530

    final fix

--- topGit/topGithub/repos.py
+++ topGit/topGithub/repos.py
@@ -6,8 +6,25 @@ def top_n_repos(username,n):
     url = 'https://api.github.com/search/repositories?q=user:' + username + '+fork:true&sort=forks&order=desc&page=1&per_page=100'
     res=requests.get(url)
     repos=res.json()
-
+    
     dict = []
+
+    if repos == {
+        "message": "Validation Failed",
+        "errors": [
+            {
+            "message": "The listed users and repositories cannot be searched either because the resources do not exist or you do not have permission to view them.",
+            "resource": "Search",
+            "field": "q",
+            "code": "invalid"
+            }
+        ],
+        "documentation_url": "https://developer.github.com/v3/search/"
+        }:
+        
+        return -1
+
+    
 
     results = 100
 
--- topGit/topGithub/views.py
+++ topGit/topGithub/views.py
@@ -11,35 +11,44 @@ def index(request):
     
     names = []
     contribute = []
-
+    context = {'check':True}
+    
     if n != None:
         lists = repos.top_n_repos(username,int(n))
-        for data in lists:
-            name = {
-                'name': data['name'],
-                'forks': data['forks']
+
+        if lists == -1:
+            context = {'check': False}
+
+        else:
+            for data in lists:
+                name = {
+                    'name': data['name'],
+                    'forks': data['forks']
+                }
+                names.append(name)
+
+
+            for i in range(int(n)):
+
+                obj = {
+                    'repoName': names[i]['name'],
+                    'values': contributers.top_m_contributers(username,names[i]['name'],int(m))
+                }
+                contribute.append(obj)
+            
+            for buf in contribute:
+                print(buf, end= "\n")
+            
+            context = {
+                'check': True,
+                'organisation': username,
+                'data': names,
+                'contribute': contribute
             }
-            names.append(name)
-
-
-        for i in range(int(n)):
-
-            obj = {
-                'repoName': names[i]['name'],
-                'values': contributers.top_m_contributers(username,names[i]['name'],int(m))
-            }
-            contribute.append(obj)
-        
-        for buf in contribute:
-            print(buf, end= "\n")
         
         
 
-    context = {
-        'organisation': username,
-        'data': names,
-        'contribute': contribute
-    }
+    
 
     return render(request,'topGithub/index.html', context)
     
